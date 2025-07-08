30ce9792be21a63175c3f34be33d7ffc7e6bfe78
thenoormughal <thenoormughal@gmail.com>
2025-07-08 20:12:05 +0500

    Botton to Top Button

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.addEventListener("load", () => {
   document.querySelector("h2").classList.add("animate");
 });
+// ===== Back to Top Button =====
+const backToTop = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
